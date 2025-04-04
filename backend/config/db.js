2c6f0e92f4b933648d243a7c53a8dfc18c0c9017
alikamatu_devrel <122170675+alikamatu@users.noreply.github.com>
2025-04-04 14:35:03 +0000

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,33 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+// db.js
+import postgres from 'postgres'
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const connectionString = process.env.DATABASE_URL
+const pool = postgres(connectionString)
+console.log('✅ Connected to PostgreSQL');
 
-pool.connect()
-  .then(client => {
-    console.log('✅ Connected to PostgreSQL');
-    return client.query('SELECT NOW()').then(res => {
-      console.log('⏱️ Current time:', res.rows[0].now);
-      client.release();
-    });
-  })
-  .catch(err => console.error('❌ Connection error:', err.message));
 
-module.exports = pool;
+export default pool
+
+
+// require("dotenv").config();
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect()
+//   .then(client => {
+//     console.log('✅ Connected to PostgreSQL');
+//     return client.query('SELECT NOW()').then(res => {
+//       console.log('⏱️ Current time:', res.rows[0].now);
+//       client.release();
+//     });
+//   })
+//   .catch(err => console.error('❌ Connection error:', err.message));
+
+// module.exports = pool;
