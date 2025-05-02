464e1bb76d8788da8319fd99397a938391497f42
alikamatu_devrel <122170675+alikamatu@users.noreply.github.com>
2025-05-02 10:29:43 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,15 @@ const adminRoutes = require("./routes/adminRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
+
+app.use(cors({
+    origin: '*',
+}));
+
 const port = process.env.PORT || 1000;
 
 app.use(express.json());
-const allowedOrigins = [
-  "https://skc-attendance-uhlu.vercel.app", // Frontend origin
-  "http://localhost:3000", // Local development frontend
-  "https://skc-attendance-46dh.vercel.app" // Backend origin
-];
 
-app.use(
-  cors({
-    origin: "https://skc-attendance-uhlu.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // If cookies or authentication are needed
-  })
-);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api", studentRoutes);
